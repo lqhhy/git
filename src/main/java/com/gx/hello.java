ad16d290587ce16cc3164bac79a3b40d576a9c0c
linlin <linlin@123456>
2023-02-28 09:29:00 +0800

    hot-fix

--- src/main/java/com/gx/hello.java
+++ src/main/java/com/gx/hello.java
@@ -8,6 +8,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("你好！！");
         System.out.println("你好！git");
+        System.out.println("你好！hot-fix");
 
     }
 }
